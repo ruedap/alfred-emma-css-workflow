6899fb9c9863ecfebdd4d9365f35909dc417f538
ruedap <ruedap@ruedap.com>
2015-04-08 23:24:51 +0900

    Refactor

---
+++ cli_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/ruedap/go-alfred"
+	"github.com/ruedap/go-emma"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCLI_Run(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./awc margin-left 0", " ")
+
+	status := cli.Run(args)
+	assert.Equal(t, status, ExitCodeOK)
+
+	expected := "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<items><item valid=\"true\" arg=\"u-ml-0\" uid=\"ml-0\"><title>.u-ml-0 { margin-left: 0; }</title><subtitle>Paste class name: u-ml-0</subtitle><icon>icon.png</icon></item></items>"
+	assert.Equal(t, outStream.String(), expected)
+}
+
+func TestCLI_Run_MinusArgs(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./awc --", " ")
+
+	status := cli.Run(args)
+	assert.Equal(t, status, ExitCodeOK)
+
+	expected := "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<items><item valid=\"true\" arg=\"u-ti--9999\" uid=\"ti--9999\"><title>.u-ti--9999 { text-indent: -9999px; }</title><subtitle>Paste class name: u-ti--9999</subtitle><icon>icon.png</icon></item><item valid=\"true\" arg=\"u-ord--1\" uid=\"ord--1\"><title>.u-ord--1 { order: -1; }</title><subtitle>Paste class name: u-ord--1</subtitle><icon>icon.png</icon></item></items>"
+	assert.Equal(t, outStream.String(), expected)
+}
+
+func TestCLI_Run_NoResult(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./awc emma.css", " ")
+
+	status := cli.Run(args)
+	assert.Equal(t, status, ExitCodeOK)
+
+	expected := "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<items></items>"
+	assert.Equal(t, outStream.String(), expected)
+}
+
+func TestCLI_find(t *testing.T) {
+	actual, err := find([]string{"foo"})
+	assert.Nil(t, err)
+
+	expected := `[{"snippet":"d-tbfg","property":"display","value":"table-footer-group"}]`
+	assert.Equal(t, actual, expected)
+}
+
+func TestCLI_response(t *testing.T) {
+	actual := response([]emma.Decl{})
+	expected := new(alfred.Response)
+	expected.Items = []alfred.ResponseItem{}
+	assert.Equal(t, actual, expected)
+}
--- main.go
+++ main.go
@@ -1,43 +1,10 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
+import "os"
 
-	"github.com/ruedap/go-alfred"
-	"github.com/ruedap/go-emma"
-)
+const Version string = "0.3.0.0"
 
 func main() {
-	e := emma.NewEmma()
-	str, err := e.Find(os.Args[1:]).ToJSON()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var decls []emma.Decl
-	err = json.Unmarshal([]byte(str), &decls)
-
-	res := alfred.NewResponse()
-	for _, d := range decls {
-		ri := alfred.ResponseItem{
-			Valid:    true,
-			Arg:      fmt.Sprintf("u-%v", d.Snippet),
-			UID:      d.Snippet,
-			Title:    fmt.Sprintf(".u-%v { %v: %v; }", d.Snippet, d.Property, d.Value),
-			Subtitle: fmt.Sprintf("Paste class name: u-%v", d.Snippet),
-			Icon:     "icon.png",
-		}
-		res.AddItem(&ri)
-	}
-
-	xml, err := res.ToXML()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(xml)
+	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
+	os.Exit(cli.Run(os.Args))
 }
