a6e385af38b9d2df6e76f6cf05ae2ac1d54d6aa7
ruedap <ruedap@ruedap.com>
2020-06-21 20:48:38 +0900

    feat: Add alfred/response

---
+++ src/alfred/response.ts
@@ -0,0 +1,26 @@
+// https://www.alfredapp.com/help/workflows/inputs/script-filter/json/
+type TItem = Readonly<{
+  uid?: string;
+  title: string;
+  subtitle: string;
+  arg: string; // recommended
+  icon: {
+    type: "fileicon" | "filetype";
+    path: string;
+  };
+  valid?: boolean;
+  match?: string;
+  autocomplete?: string; // recommended
+  type?: "default" | "file" | "file:skipcheck";
+  mods?: object;
+  text?: object;
+  quicklookurl?: string;
+}>;
+
+export type TResponse = Readonly<{
+  items: TItem[];
+}>;
+
+export const toJson = (response: TResponse): string => {
+  return JSON.stringify(response);
+};
