8f39fc6b1d758f80835cefc032ce1082f7652020
ruedap <ruedap@ruedap.com>
2020-06-21 21:32:28 +0900

    refactor: Implement getQuery()

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { parse } from "https://deno.land/std/flags/mod.ts";
 import { getData } from "./emma.ts";
+import { getQuery } from "./query.ts";
 import { search } from "./search.ts";
 
 const data = getData();
-const query = parse(Deno.args)._.join(" ");
-const result = search(
+const query = getQuery();
+const searchResult = search(
   data.rules.props,
   ["name", "values.name"],
   query,
 );
 
-console.log(result);
+console.log(searchResult);
